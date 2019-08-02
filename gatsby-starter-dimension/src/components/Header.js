ccf7ebbe3ef249b5cb490e48b8fa0af3f0f84dfe
bobbyvidal <bobbyvidal@gmail.com>
2019-08-02 01:25:58 -0500

    first commit

---
+++ gatsby-starter-dimension/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span class="icon fa-keyboard-o"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Bobby Vidal </h1>
+                <p>Full-stack developer and graduate from the University of Houston.</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
