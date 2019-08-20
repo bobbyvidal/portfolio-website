42e16ea986557fdf79dae71df8b3ee04b9ca051d
bobbyvidal <bobbyvidal@gmail.com>
2019-08-20 15:32:30 -0500

    using sendgrid to send emails

--- server/index.js
+++ server/index.js
@@ -16,31 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/contact', (req, res) => {
   console.log(req.body)
 
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'bobbyvidalportfoliowebsite@gmail.com',
-      pass: process.env.PASSWORD
-    },
-  });
   
-  var mailOptions = {
-    from: 'bobbyvidalportfoliowebsite@gmail.com',
-    to: 'bobbyvidal@gmail.com',
-    subject: `Sent by ${req.body.name} - ${req.body.email}`,
-    text: `${req.body}`
   };
   
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
   
-})
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
