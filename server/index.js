f86f870739f5c7a3ef466579ccbd18ade1ddbdbf
bobbyvidal <bobbyvidal@gmail.com>
2019-08-21 18:35:17 -0500

    committing my email branch

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const app = express()
 var cors = require('cors')
+const sgMail = require('@sendgrid/mail');
 app.use(cors())
 const bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
@@ -9,19 +10,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('*', (req, res) => {
-//   res.send('Server is working. Please post at "/contact" to submit a message.')
-// })
+app.get('*', (req, res) => {
+  res.send('Server is working. Please post at "/contact" to submit a message.')
+})
 
 app.post('/contact', (req, res) => {
   console.log(req.body)
-
-  
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'bobbyvidal@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
-  
-  
+  sgMail.send(msg);
+})
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 })
+
+
